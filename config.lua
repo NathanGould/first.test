e23f12e3ba24f5a81e059e9e3a821e46e780ac92
Nathan <gouldnathan0@gmail.com>
2013-07-28 02:32:56 -0400

    initial commit

---
+++ config.lua
@@ -0,0 +1,29 @@
+application = {
+	content = {
+		width = 800,
+		height = 600, 
+		scale = "letterBox",
+		fps = 30,
+		
+		--[[
+        imageSuffix = {
+		    ["@2x"] = 2,
+		}
+		--]]
+	},
+
+    --[[
+    -- Push notifications
+
+    notification =
+    {
+        iphone =
+        {
+            types =
+            {
+                "badge", "sound", "alert", "newsstand"
+            }
+        }
+    }
+    --]]    
+}
